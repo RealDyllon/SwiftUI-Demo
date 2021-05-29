921442f31c6cb5425c9939712f3412a970a84845
Dyllon <dyllon.g@outlook.com>
2021-05-30 00:13:41 +0800

    Add OrderView Screen

---
+++ iDine/OrderView.swift
@@ -0,0 +1,43 @@
+//
+//  OrderView.swift
+//  iDine
+//
+//  Created by Dyllon on 29/5/21.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    @EnvironmentObject var order: Order
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    ForEach(order.items) { item in
+                        HStack {
+                            Text(item.name)
+                            Spacer()
+                            Text("$\(item.price)")
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination: Text("Check Out")) {
+                        Text("Place Order")
+                    }
+                }
+            }
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Order")
+        }
+    }
+}
+
+struct OrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderView()
+            .environmentObject(Order())
+    }
+}
