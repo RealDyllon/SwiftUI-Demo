5e9d5c5cb4fb50559f4a83f45f2b3ede6cb23a4e
Dyllon <dyllon.g@outlook.com>
2021-05-29 20:18:32 +0800

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Dyllon on 29/5/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
